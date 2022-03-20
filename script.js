1da287af07fdc662b613d297165e8b45eb188cd5
Vasiliy2020789 <89048929616@mail.ru>
2022-03-21 01:02:50 +0700

    form created
    Получаем данные для расчета из формы

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+//Получаем элементы формы
+const parametersOfСalc = document.forms.parametersOfСalc;//получение конкретной формы по атрибуту "name"
+const parametersOfСalcPeriod = parametersOfСalc.period;
+
+parametersOfСalcPeriod[0].value = "day";
+parametersOfСalcPeriod[1].value = "week";
+const parametersOfСalcDelta = parametersOfСalc.delta;
+const parametersOfСalcDuration = parametersOfСalc.duration;
+const button = parametersOfСalc.button;//type="submit"
+console.log(button);
 //сюда будем вставлять элементы таблицы дни/суммы
 const tabs = document.querySelector('.tabs');
 // сда будем получать данные для расчета
 //let delta, days;
-let begin = 1;
-let end = 365;
-let delta = 5;
-let sum = 0;
-console.log(`Скрипт подключен`);
-
-for (i = 0; i < (end + 1); i++) {
-	sum = sum + i * delta;
+button.addEventListener("click", function (event) {//при событии "click" выполняем функцию
+	let begin = Number(parametersOfСalcDelta.value);
+	let end = Number(parametersOfСalcDuration.value);
+	let delta = Number(parametersOfСalcDelta.value);
+	console.log(begin);
+	console.log(end);
+	console.log(delta);
+	let sum = 0;
+	Number()
+	for (i = 0; i < (end + 1); i++) {
+		sum = sum + i * delta;
+		//console.log(sum);
+	}
 	console.log(sum);
-}
+	tabs.insertAdjacentHTML(
+		"beforebegin",
+		`<br> За  ${end} дней, если каждый день увеличивать сумму пополнения копилки на ${delta} рублей у Вас накопится ${sum} рублей.`
+	);
+	event.preventDefault()
+});
 
-console.log(`сумма чисел от ${begin} до ${end} с шагом ${delta} равна ${sum}`);
-tabs.insertAdjacentHTML(
-	"beforebegin",
-	`сумма чисел от ${begin} до ${end} с шагом ${delta} равна ${sum}`
-);
+
+
+//console.log(`сумма чисел от ${begin} до ${end} с шагом ${delta} равна ${sum}`);
+
 //tabs.innerHTML = `сумма чисел от ${begin} до ${end} с шагом ${delta} равна ${sum}`;
