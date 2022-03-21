16e2abcb8a073cf520d1fc32570dd121e54636f9
Vasiliy2020789 <89048929616@mail.ru>
2022-03-21 18:42:30 +0700

    h90%c20%j50%p0%
    Готово:
    форма
    вычисление накоплений
    формирование клеток-дней
    форма и правила скрываются

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-//Получаем элементы формы
+//_______Получаем элементы навигации и скрывающиеся блоки
+const headerLinkGuide = document.querySelector(".header__link_guide");
+const headerLinkShowForm = document.querySelector(".header__link_show-form");
+const headerLinkEntrance = document.querySelector(".header__link_entrance");
+const headerLinkRegistration = document.querySelector(".header__link_registration");
+const guide = document.querySelector(".guide");
+//__end__Получаем элементы навигации и скрывающиеся блоки
+
+//_______Получаем элементы формы
 const parametersOfСalc = document.forms.parametersOfСalc;//получение конкретной формы по атрибуту "name"
 const parametersOfСalcPeriod = parametersOfСalc.period;
 
@@ -7,12 +15,29 @@ parametersOfСalcPeriod[1].value = "week";
 const parametersOfСalcDelta = parametersOfСalc.delta;
 const parametersOfСalcDuration = parametersOfСalc.duration;
 const button = parametersOfСalc.button;//type="submit"
-console.log(button);
+//__end__Получаем элементы формы
+
+//_______Получаем блоки в которые будем вставлять значения
 //сюда будем вставлять элементы таблицы дни/суммы
+const сalculations = document.querySelector(".сalculations");//<div class="results"></div><div class="current-sum"></div><div class="rest-of-days"></div>
 const tabs = document.querySelector('.tabs');
-// сда будем получать данные для расчета
+// сюда будем получать данные для расчета
 //let delta, days;
+//_______Функционал кнопок headera
+headerLinkGuide.addEventListener("click", function (event) {
+	guide.classList.toggle('hide');
+	event.preventDefault();
+})
+
+headerLinkShowForm.addEventListener("click", function (event) {
+	parametersOfСalc.classList.toggle('hide');
+	event.preventDefault();
+})
+//__end__Функционал кнопок headera
+
+
 button.addEventListener("click", function (event) {//при событии "click" выполняем функцию
+	event.preventDefault()
 	let begin = Number(parametersOfСalcDelta.value);
 	let end = Number(parametersOfСalcDuration.value);
 	let delta = Number(parametersOfСalcDelta.value);
@@ -20,19 +45,24 @@ button.addEventListener("click", function (event) {//при событии "clic
 	console.log(end);
 	console.log(delta);
 	let sum = 0;
-	Number()
-	for (i = 0; i < (end + 1); i++) {
+	for (i = 0; i < end; i++) {
 		sum = sum + i * delta;
 		//console.log(sum);
+		tabs.insertAdjacentHTML(
+			"beforebegin",
+			`<div class="tabs__item">
+				<div class="tabs__days">День ${i + 1}-й</div>
+				<div class="tabs__sum">${(i + 1) * delta} рублей</div>
+			</div>`
+		);
 	}
-	console.log(sum);
-	tabs.insertAdjacentHTML(
-		"beforebegin",
-		`<br> За  ${end} дней, если каждый день увеличивать сумму пополнения копилки на ${delta} рублей у Вас накопится ${sum} рублей.`
+	//console.log(sum);
+	сalculations.insertAdjacentHTML(
+		"beforeend",
+		`За  ${end} дней, если каждый день увеличивать сумму пополнения копилки на ${delta} рублей у Вас накопится ${sum} рублей.`
 	);
-	event.preventDefault()
+	parametersOfСalc.classList.toggle('hide');
 });
-
 
 
 //console.log(`сумма чисел от ${begin} до ${end} с шагом ${delta} равна ${sum}`);
